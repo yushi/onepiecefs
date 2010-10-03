d40f85ba041255b277a2554a61acfba526648425
Yushi Nakai <yushi.nakai@gmail.com>
2010-10-03 16:30:00 +0900

    fixed install path

--- prototype/build_osx.sh
+++ prototype/build_osx.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
-
+INSTALL_PATH=/opt/opfs
 PWD=`pwd`
 WORK_DIR=${PWD}/opfsbuild
 PYTHON_DL_URL="http://www.python.org/ftp/python/2.7/Python-2.7.tar.bz2"
 FUSE_PYTHON_URL="http://pypi.python.org/packages/source/f/fuse-python/fuse-python-0.2.tar.gz#md5=68be744e71a42cd8a92905a49f346278"
-INSTALL_PATH=`echo ~/opfs`
+
+mkdir -p /opt/opfs
+if [ $? -eq 1 ];then
+    echo "can't write ${INSTALL_PATH}. please make writable (ex. sudo mkdir -p /opt/opfs && sudo chown you /opt/opfs)"
+    exit
+fi
+
 if [ ! -d /Library/Frameworks/MacFUSE.framework ];then
     echo -e "MacFuse not installed\n\thttp://code.google.com/p/macfuse/"
     exit
@@ -22,7 +28,6 @@ cd Python-2.7
 ./configure --prefix=${INSTALL_PATH}/python27
 make && make install
 cd ..
-
 
 export CFLAGS_TMP=$CFLAGS
 export CFLAGS="${CFLAGS} -I${WORK_DIR}/include"
